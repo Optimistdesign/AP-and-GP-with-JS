27e3b2d55f1e1ea406cb5de03ac7ee634c1c4bd2
Optimist.jsx <101150525+Optimistdesign@users.noreply.github.com>
2022-06-17 19:45:09 +0100

    Add files via upload

---
+++ ap-gp.js
@@ -0,0 +1,62 @@
+function checkAp(afirst, anum, adiff){
+    var afirst = parseInt(afirst);  
+    var anum = parseInt(anum);
+    var adiff = parseInt(adiff);
+
+    var arithmetic = afirst + adiff*(anum-1);
+
+    return arithmetic;
+}
+function checkGp(first, num, diff){
+    var first = parseInt(first);
+    var num = parseInt(num);
+    var diff = parseInt(diff);
+
+    var Geometric = first * Math.pow(diff, num - 1);
+    return Geometric;
+}
+
+function findAp(event){
+    event.preventDefault();
+    const message = document.getElementById('message1');
+    try{
+        message.innerText = "";
+
+        const Np = document.getElementById('anum').value;
+        const Rp = document.getElementById('adiff').value;
+        const Ap = document.getElementById('afirst').value;
+
+        if(Np === "" || Rp === "" || Rp === ""){
+            throw new Error("All Input Parameters are required")
+        }
+        alert(checkAp(Np, Rp, Ap));
+        
+    }catch(err){
+        message.innerText = err.message;
+        console.error(err)
+    }
+    
+}
+
+
+function findGp(event){
+    event.preventDefault();
+
+    message = document.getElementById('message2');
+    try {
+        message.innerText = "";
+
+        const A = document.getElementById('first').value;
+        const N = document.getElementById('num').value;
+        const R = document.getElementById('diff').value;
+
+        if(A === "" || N === "" || R === ""){
+            throw new Error("All Input Parameters are required")
+        }
+        alert(checkGp(A, N, R));
+
+    }catch(err){
+        message.innerText = err.message;
+        console.error(err)
+    }
+}
